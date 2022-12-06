a220c54fb0ebfbec9e6f50837381e48e0406f963
Valera Drokin <drokin.valera@gmail.com>
2022-12-06 13:53:47 +0300

    Add main method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class Main {
+
+    public static void main(String[] args) throws Exception {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            String input = reader.readLine();
+
+            String output = calc(input);
+
+            writer.write(output);
+        }
+    }
 
     public static String calc(String input) throws Exception {
         Equation equation = new Equation(input);
